d4f7a999e3ed58d984409c69c15d5c87e0a54f0f
wansho <wanshojs@gmail.com>
2022-07-26 18:52:50 +0800

    update

---
+++ basic/template/index.js
@@ -0,0 +1,11 @@
+const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+console.log(p.replace('dog', 'monkey'));
+// expected output: "The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?"
+
+const regex = /Dog/i;
+console.log(p.replace(regex, 'ferret'));
+// expected output: "The quick brown fox jumps over the lazy ferret. If the dog reacted, was it really lazy?"
+
+// replace 只替换第一个匹配项！
+
+// replaceAll 可以替换所有匹配项，但是 IE 不兼容！
