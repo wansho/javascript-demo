e9fff4111f677ab62bdf9102369d16c038bb7ac6
wansho <wanshojs@gmail.com>
2022-07-28 08:24:16 +0800

    update

--- basic/template/index.js
+++ basic/template/index.js
@@ -9,3 +9,20 @@ console.log(p.replace(regex, 'ferret'));
 // replace 只替换第一个匹配项！
 
 // replaceAll 可以替换所有匹配项，但是 IE 不兼容！
+// 兼容 IE11
+var operatorRegex = /[\+\-\(\)\*/]|(abs\(.+\))/ig;
+var expression = 'e+f+g+abs(c)+abs(d)';
+var absRegex = /abs\([a-z]\)/g;
+var absPlaceholder = '!!!';
+var absList = expression.match(absRegex)
+expression = expression.split(absRegex).join(absPlaceholder);
+var operatorList = ['+', '-', '*', '/', '(', ')']
+for(var i=0; i<operatorList.length; i++){
+  var operator = operatorList[i];
+  expression = expression.split(operator).join(' ' + operator + ' ');
+}
+for(var j=0; j<absList.length; j++){
+  expression = expression.replace(absPlaceholder, absList[j]);
+}
+expression = expression.trim()
+console.log(expression)
